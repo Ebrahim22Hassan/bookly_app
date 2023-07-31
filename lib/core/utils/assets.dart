a3b1654fdb5b2f0725cbf0cea692718c8f4367e3
Ebrahim22Hassan <110447959+Ebrahim22Hassan@users.noreply.github.com>
2023-08-01 07:18:55 +0800

    add custom listview image

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,4 +1,5 @@
 class AssetsData {
   static const logoL = "assets/images/Logo_L.png";
   static const logoD = "assets/images/Logo_D.png";
+  static const testImage = "assets/images/test_image.png";
 }
