bdce194780b5a63dfc3850a0334f92b46f8ec1b0
Ebrahim22Hassan <110447959+Ebrahim22Hassan@users.noreply.github.com>
2023-08-03 06:55:46 +0800

    finish best seller listview

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:bookly/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
+import '../../../../../constants.dart';
 import '../../../../../core/utils/styles.dart';
+import 'best_seller_list_view.dart';
 import 'featured_books_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -8,22 +10,37 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-          CustomAppBar(),
-          FeaturedBooksListView(),
-          SizedBox(
-            height: 50,
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: kPadding30),
+                child: CustomAppBar(),
+              ),
+              FeaturedBooksListView(),
+              SizedBox(
+                height: 50,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: kPadding30),
+                child: Text("Best Seller", style: Styles.textStyle18),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+            ],
           ),
-          Text("Best Seller", style: Styles.textStyle18),
-          SizedBox(
-            height: 20,
+        ),
+        const SliverFillRemaining(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: kPadding30),
+            child: BestSellerListView(),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
