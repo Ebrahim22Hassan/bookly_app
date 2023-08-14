807734353e359137685bdf6fe3f0ec2bf93347a5
Ebrahim22Hassan <110447959+Ebrahim22Hassan@users.noreply.github.com>
2023-08-14 16:43:25 +0800

    generate book entity adapter

--- lib/features/home/domain/entities/book_entity.dart
+++ lib/features/home/domain/entities/book_entity.dart
@@ -1,8 +1,21 @@
+import 'package:hive/hive.dart';
+
+/// add this
+part 'book_entity.g.dart';
+
+/// then Run this command "flutter packages pub run build_runner build"
+
+@HiveType(typeId: 0)
 class BookEntity {
+  @HiveField(0)
   final String bookId;
+  @HiveField(1)
   final String? image;
+  @HiveField(2)
   final String title;
+  @HiveField(3)
   final String? authorName;
+  @HiveField(4)
   final num? rating;
 
   BookEntity(
