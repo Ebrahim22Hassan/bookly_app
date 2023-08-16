0bcd49c49590c942dc10a26d824a78554ac16e79
Ebrahim22Hassan <110447959+Ebrahim22Hassan@users.noreply.github.com>
2023-08-16 11:53:35 +0800

    create list to save fetched books

--- lib/features/home/presentation/views/widgets/featured_books_list_view_bloc_builder.dart
+++ lib/features/home/presentation/views/widgets/featured_books_list_view_bloc_builder.dart
@@ -3,19 +3,32 @@ import 'package:bookly/core/widgets/custom_error_widget.dart';
 import 'package:bookly/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../domain/entities/book_entity.dart';
 import 'featured_books_list_view.dart';
 
-class FeaturedBooksListViewBlocBuilder extends StatelessWidget {
+class FeaturedBooksListViewBlocBuilder extends StatefulWidget {
   const FeaturedBooksListViewBlocBuilder({Key? key}) : super(key: key);
 
   @override
+  State<FeaturedBooksListViewBlocBuilder> createState() =>
+      _FeaturedBooksListViewBlocBuilderState();
+}
+
+class _FeaturedBooksListViewBlocBuilderState
+    extends State<FeaturedBooksListViewBlocBuilder> {
+  List<BookEntity> books = [];
+  @override
   Widget build(BuildContext context) {
-    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
-        builder: (context, state) {
+    return BlocConsumer<FeaturedBooksCubit, FeaturedBooksState>(
+        listener: (context, state) {
+      if (state is FeaturedBooksSuccess) {
+        books.addAll(state.books);
+      }
+    }, builder: (context, state) {
       if (state is FeaturedBooksSuccess ||
           state is FeaturedBooksPaginationLoading) {
         return FeaturedBooksListView(
-          books: state is FeaturedBooksSuccess ? state.books : [],
+          books: books,
         );
       } else if (state is FeaturedBooksFailure) {
         return CustomErrorWidget(errorMessage: state.errMessage);
